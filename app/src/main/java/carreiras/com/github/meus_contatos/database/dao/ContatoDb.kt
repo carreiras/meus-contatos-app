f3adb5747ac83a2cd6f8fba29b0a677bc7a7c4be
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-03-10 15:41:59 -0300

    Refatoração - Excluindo comentários

--- app/src/main/java/carreiras/com/github/meus_contatos/database/dao/ContatoDb.kt
+++ app/src/main/java/carreiras/com/github/meus_contatos/database/dao/ContatoDb.kt
@@ -8,25 +8,6 @@ import carreiras.com.github.meus_contatos.model.Contato
 
 @Database(entities = [Contato::class], version = 1)
 abstract class ContatoDb : RoomDatabase() {
-//    abstract fun contatoDao(): ContatoDao
-//    companion object {
-//        private lateinit var instance: ContatoDb
-//        fun getDatabase(context: Context): ContatoDb {
-//            if (!::instance.isInitialized) {
-//                instance = Room
-//                    .databaseBuilder(
-//                        context,
-//                        ContatoDb::class.java,
-//                        "contato_db"
-//                    )
-//                    .allowMainThreadQueries()
-//                    .fallbackToDestructiveMigration()
-//                    .build()
-//            }
-//            return instance
-//        }
-//    }
-
     abstract fun contatoDao(): ContatoDao
 
     companion object {

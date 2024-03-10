6e66a7ffac741a59649781e4ba4f0b9fe41acc47
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-03-10 14:09:22 -0300

    Criação do repositório de contatos

---
+++ app/src/main/java/carreiras/com/github/meus_contatos/database/repository/ContatoRepository.kt
@@ -0,0 +1,24 @@
+package carreiras.com.github.meus_contatos.database.repository
+
+import android.content.Context
+import carreiras.com.github.meus_contatos.database.dao.ContatoDb
+import carreiras.com.github.meus_contatos.model.Contato
+
+class ContatoRepository(context: Context) {
+    private val db = ContatoDb.getDatabase(context).contatoDao()
+    fun salvar(contato: Contato): Long {
+        return db.salvar(contato)
+    }
+    fun atualizar(contato: Contato): Int {
+        return db.atualizar(contato)
+    }
+    fun excluir(contato: Contato): Int {
+        return db.excluir(contato)
+    }
+    fun listarContatos(): List<Contato> {
+        return db.listarContatos()
+    }
+    fun buscarContatoPeloId(id: Int): Contato {
+        return db.buscarContatoPeloId(id)
+    }
+}
